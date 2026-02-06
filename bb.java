5be98c5b4ff84ca81115a4522b2cc64bab727bfc
Rohit Patidar <rbpatidar2003@gmail.com>
2026-02-06 22:11:46 +0530

    Update bb.java

--- bb.java
+++ bb.java
@@ -5,16 +5,16 @@ class bb
 	{
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Array Size"):
+		System.out.println("Enter the Array Size");		
 		int s=sc.nextInt();
-		int[] a=new int[s]
+		int[] a=new int[s];
 			int  n=a.length ;
 		System.out.println("Enter th array element ");
 		for(int i=0;i<n;i++)
 			{
 				a[i]=sc.nextInt();
 			}
-		for(int i=0 ; i<n-1;i++)
+		for(int i=0 ;i<n-1;i++)
 		{
 			for(int j=0; j<n-1-i; j++)
 			{
@@ -34,4 +34,5 @@ class bb
 	}
 
 }
+
 
