2f923e199a6c2c86b71adb41b008d8e78fc500dd
Rohit Patidar <rbpatidar2003@gmail.com>
2026-02-06 22:07:04 +0530

    Modify bubble sort to accept user input
    Updated the bubble sort program to take user input for array size and elements.

--- bb.java
+++ bb.java
@@ -1,9 +1,19 @@
+import java.util.*;
 class bb
 {
 	public static void main (String ar[])
 	{
 		int[] a={-8,5,4,1,11,7,6,2};
-		int  n=a.length ;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Array Size"):
+		int s=sc.nextInt();
+		int[] a=new int[s]
+			int  n=a.length ;
+		System.out.println("Enter th array element ");
+		for(int i=0;i<n;i++)
+			{
+				a[i]=sc.nextInt();
+			}
 		for(int i=0 ; i<n-1;i++)
 		{
 			for(int j=0; j<n-1-i; j++)
@@ -22,4 +32,5 @@ class bb
 		}
 		
 	}
+
 }
